b4e4da89232d16af96fb5cce3ec33100a370f7f6
manor hagage <manorhagage@gmail.com>
2021-02-03 18:09:18 +0200

    responsive style

--- src/components/ContactUs/components/Branch/Branch.js
+++ src/components/ContactUs/components/Branch/Branch.js
@@ -3,10 +3,8 @@ import './Branch.css';
 function Branch() {
     return (
         <div className='Branche'>
-            <h2> Rothschild street 78, Rishon Lezion </h2>
-            <div className='map'>
-                <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3384.874103957787!2d34.79405311516159!3d31.96431198122502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502b43fd4852dd5%3A0x735e0e32c170d3ec!2sRothschild%20St%2078%2C%20Rishon%20LeTsiyon!5e0!3m2!1sen!2sil!4v1612356862728!5m2!1sen!2sil'></iframe>
-            </div>
+            <h2 className='address'> Rothschild street 78, Rishon Lezion </h2>
+            <iframe className='map' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3384.874103957787!2d34.79405311516159!3d31.96431198122502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502b43fd4852dd5%3A0x735e0e32c170d3ec!2sRothschild%20St%2078%2C%20Rishon%20LeTsiyon!5e0!3m2!1sen!2sil!4v1612356862728!5m2!1sen!2sil'></iframe>
         </div>
     )
 }
