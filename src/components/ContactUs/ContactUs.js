410201604509428438d437d9e71daeb4d4d652bd
manor hagage <manorhagage@gmail.com>
2021-02-03 16:17:13 +0200

    add

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,10 +1,15 @@
 import { Branch } from './components/Branch';
 import './ContactUs.css';
+import ContactUsSvg from '../../static/contact-us.svg'
 
 function ContactUs() {
     return (
         <div className='contact-us'>
-            <h1> Contact Us </h1>
+            <div className='title'>
+                <h1> Contact Us </h1>
+                <img className='svg' src={ContactUsSvg} />
+            </div>
+
             <div className='Branches'>
                 
                 <Branch />
