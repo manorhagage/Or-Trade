3e55d799bb7929e94c2f2e5dfa5bd3b0d4044f17
manor hagage <manorhagage@gmail.com>
2021-02-03 12:05:04 +0200

    finish Home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useState } from 'react';
+
 import './App.css';
+import { Home } from './components/Home';
 
 function App() {
+  const [activePage, setActivePage] = useState('Home');
+
   return (
     <div className="App">
-      
+      <Home activePage={activePage} />
     </div>
   );
 }
