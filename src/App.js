e86d032d936ff3985a972406322635211f7e8e2d
manor hagage <manorhagage@gmail.com>
2021-02-03 20:42:31 +0200

    Design improvements

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import { Home } from './components/Home';
 import { OurServices } from './components/OurServices';
 
 function App() {
-  const [activePage, setActivePage] = useState('Home');
+    const [activePage, setActivePage] = useState('Home');
 
-  return (
-    <div className="App">
-      <Home activePage={activePage} />
-      <About />
-      <OurServices />
-      <ContactUs />
-    </div>
-  );
+    return (
+      <div className="App">
+        <Home activePage={activePage} />
+        <About />
+        <OurServices />
+        <ContactUs />
+      </div>
+    );
 }
 
 export default App;
