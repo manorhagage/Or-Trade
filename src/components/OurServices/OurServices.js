e7b0f7d914f221eed2330f452027ea09c7e6d192
manor hagage <manorhagage@gmail.com>
2021-02-03 13:23:56 +0200

    add content & css

--- src/components/OurServices/OurServices.js
+++ src/components/OurServices/OurServices.js
@@ -1,9 +1,36 @@
 
+import './OurServices.css'
 
 function OurServices() {
     return (
-        <div className='OurServices'>
-            <span className='service'></span>
+        <div className='our-services'>
+            <div className='service'>
+                <h2> Just An Example </h2>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil amet eum dolores sit veritatis iusto vero itaque quia. Eaque laudantium voluptate culpa nisi veniam, inventore obcaecati temporibus error sit debitis? 
+                </p>
+            </div>
+            
+            <div className='service'>
+                <h2> Just An Example </h2>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil amet eum dolores sit veritatis iusto vero itaque quia. Eaque laudantium voluptate culpa nisi veniam, inventore obcaecati temporibus error sit debitis? 
+                </p>
+            </div>
+            
+            <div className='service'>
+                <h2> Just An Example </h2>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil amet eum dolores sit veritatis iusto vero itaque quia. Eaque laudantium voluptate culpa nisi veniam, inventore obcaecati temporibus error sit debitis? 
+                </p>
+            </div>
+            
+            <div className='service'>
+                <h2> Just An Example </h2>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil amet eum dolores sit veritatis iusto vero itaque quia. Eaque laudantium voluptate culpa nisi veniam, inventore obcaecati temporibus error sit debitis? 
+                </p>
+            </div>
             
         </div>
     )
